fa8c196873ef4969ac4c5310b1d7bc77577739f3
Kyle Erhabor <kyleerhabor@gmail.com>
2024-04-14 10:33:51 -0400

    Initial Commit

---
+++ Gateway/GatewayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatewayApp.swift
+//  Gateway
+//
+//  Created by Kyle Erhabor on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GatewayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
